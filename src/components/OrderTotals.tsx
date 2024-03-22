6b4cdb632cdb0cc9feb0dd325d59665fe1175b2a
Antonio <antonio.ibarra99@outlook.es>
2024-03-22 14:43:42 -0700

    tip_calculator

---
+++ src/components/OrderTotals.tsx
@@ -0,0 +1,53 @@
+import { useMemo } from 'react';
+import type { OrderItem } from '../types';
+import formatCurrency from '../helpers';
+
+type OrderTotalProps = {
+  order: OrderItem[];
+  tip: number;
+  placeOrder: () => void;
+};
+
+export default function OrderTotals({
+  order,
+  tip,
+  placeOrder,
+}: OrderTotalProps) {
+  const subtotalAmount = useMemo(
+    () => order.reduce((total, item) => total + item.quantity * item.price, 0),
+    [order]
+  );
+
+  const tipAmount = useMemo(() => subtotalAmount * tip, [tip, order]);
+
+  const totalAmout = useMemo(
+    () => subtotalAmount + tipAmount,
+    [order, tipAmount]
+  );
+
+  return (
+    <>
+      <div className="space-y-3">
+        <h2 className="font-black text-2xl">Totales y propina:</h2>
+        <p>
+          Subtotal a pagar: {''}
+          <span className="font-bold">{formatCurrency(subtotalAmount)}</span>
+        </p>
+        <p>
+          Propina: {''}
+          <span className="font-bold">{formatCurrency(tipAmount)}</span>
+        </p>
+        <p>
+          Total a Pagar: {''}
+          <span className="font-bold">{formatCurrency(totalAmout)}</span>
+        </p>
+      </div>
+      <button
+        className="w-full bg-black p-3 uppercase text-white font-bold mt-10"
+        onClick={() => placeOrder()}
+      >
+        Guardar orden
+      </button>
+    </>
+  );
+}
